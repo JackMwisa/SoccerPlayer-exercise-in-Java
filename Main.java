3fb7b247cb0280f2e593c87bcd0f3b2abc644984
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-09-30 16:26:04 +0300

    no more errors

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         SoccerPlayer player1 = new SoccerPlayer("Messi", 36, "Inter Miami", 850);
         SoccerPlayer player2 = new SoccerPlayer("CR7", 38, "Al Nassr", 901);
         SoccerPlayer player3 = new SoccerPlayer("Neymar", 31, "Al Hilal", 450);
-        SoccerPlayer player4 = new SoccerPlayer("Villa", 38, "Barca", 250);\
+        SoccerPlayer player4 = new SoccerPlayer("Villa", 38, "Barca", 250);
 
         // Constructor without a team
         SoccerPlayer player5 = new SoccerPlayer("Salah", 33, 200);
@@ -22,6 +22,6 @@ public class Main {
         SoccerPlayer player7 = new SoccerPlayer("CR7", 39, "Al-Nassr", 901);
 
 
-        System.out.println(player6.equals(player7)); /
+        System.out.println(player6.equals(player7)); // trying using equals method
     }
 }
